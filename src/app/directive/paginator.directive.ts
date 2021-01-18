822d2377c708a74342c602a972e8dd0bdf46e198
pkeyur18 <patelkeyur941@gmail.com>
2021-01-19 00:35:58 +0530

    Simple App Paginator completed

---
+++ src/app/directive/paginator.directive.ts
@@ -0,0 +1,38 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appPaginator]',
+  exportAs: 'appPagination',
+})
+export class PaginatorDirective {
+
+  @Input() totalPage: number;
+  @Input() pageNumber: number = 10;
+  previous: number = 9;
+  next:number = 11;
+  current:number = 10;
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  nextPage() {
+    this.setPage(Math.min(this.totalPage, this.pageNumber + 1));
+  }
+
+  previousPage() {
+    this.setPage(Math.max(1, this.pageNumber - 1));
+  }
+  firstPage() {
+    this.setPage(1);
+  }
+  lastPage() {
+    this.setPage(this.totalPage);
+  }
+
+  setPage(pageNumber: number) {
+    this.pageNumber = pageNumber;
+    this.renderer.setProperty(this.el.nativeElement, 'value', pageNumber);
+    this.current = this.pageNumber;
+    this.previous = this.current - 1;
+    this.next = this.current + 1;
+  }
+}
